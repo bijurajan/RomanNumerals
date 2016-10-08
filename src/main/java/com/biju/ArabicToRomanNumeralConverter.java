ef186122c143ac3299fb1da6b9227912a0ce3db7
Biju Rajan <bijurajan78@gmail.com>
2016-10-07 20:36:10 -0400

    More refactoring. Getting rid of the unnecessary map

--- src/main/java/com/biju/ArabicToRomanNumeralConverter.java
+++ src/main/java/com/biju/ArabicToRomanNumeralConverter.java
@@ -1,25 +1,12 @@
 package com.biju;
 
-import java.util.Map;
-
-import com.google.common.collect.ImmutableMap;
-
 public class ArabicToRomanNumeralConverter {
 	
-	Map<Integer, String> cutOffsMap = ImmutableMap.of(
-			1, "I",
-			5, "V",
-			10, "X"
-			);
-
 	public String convert(int number) {
 		StringBuilder result = new StringBuilder();
-		if(cutOffsMap.containsKey(number)){
-			return cutOffsMap.get(number);
-		}
-		
+
 		int difference = number;
-		if(number > 10){
+		if(number >= 10){
 			result.append("X");
 			difference = difference - 10;
 		}
@@ -27,7 +14,7 @@ public class ArabicToRomanNumeralConverter {
 			result.append("IX");
 			difference = difference - 9;
 		}
-		if(difference > 5){
+		if(difference >= 5){
 			result.append("V");
 			difference = difference - 5;
 		} else if(difference == 4){
