121fc85d8749e8e58015a30d2857482c00a88e4f
Biju Rajan <bijurajan@MBP-BRAJAN.local>
2016-10-19 11:31:09 -0400

    Pulling magic numbers into constants

--- src/main/java/com/biju/ArabicToRomanNumeralConverter.java
+++ src/main/java/com/biju/ArabicToRomanNumeralConverter.java
@@ -2,6 +2,7 @@ package com.biju;
 
 public class ArabicToRomanNumeralConverter {
 
+	private static final int MAX_NUMBER_OF_CONSECUTIVE_CHARS = 3;
 	private static final int[] CUTOFF_NUMBERS = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
 	private static final String[] CUTOFF_VALUES = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
 
@@ -13,8 +14,8 @@ public class ArabicToRomanNumeralConverter {
 		for (int i = CUTOFF_NUMBERS.length - 1; i >= 0; i--) {
 			int cutOffNumber = CUTOFF_NUMBERS[i];
 			String cutOffValue = CUTOFF_VALUES[i];
-			
-			for(int j=0; j< 3 && difference >= cutOffNumber; j++){
+
+			for (int j = 0; j < MAX_NUMBER_OF_CONSECUTIVE_CHARS && difference >= cutOffNumber; j++) {
 				result.append(cutOffValue);
 				difference = difference - cutOffNumber;
 			}
